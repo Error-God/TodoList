57de7e74f3b811ac6629f96b5230febbea417f07
Error-God <kumargovinda08@gmail.com>
2024-07-17 15:34:01 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kumar, Govinda on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
